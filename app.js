b71409a9b84d26c05677785a6b5f35d6d50f63b5
Michaela <michaela.alfoldiova@gmail.com>
2020-02-21 17:01:07 +0100

    install router + pug

--- app.js
+++ app.js
@@ -1,1 +1,29 @@
 // le point d'entrée du serveur web
+const express = require('express'),
+      app = express(),
+      PORT = process.env.PORT || 3000,
+      bodyParser = require('body-parser');
+
+app.set('views', './views');
+app.set('view engine', 'pug');
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended:true}));
+
+// app.use('/players', );
+// app.use('/games', );
+
+app.get('/', function (req, res) {
+    // res.send('Hello World');
+    // res.render('main',{
+    //     title: 'Bonjour!',
+    //     name: 'Michaela',
+    //     content: 'and now everything goes my way'
+    // });
+    res.format({html: () => { res.send('<p>Bonjour !</p>') },json: () => { res.send({ message:'Bonjour !' }) }})
+});
+
+
+app.listen(PORT, () => {
+    console.log('Server up and running on localhost:', PORT);
+});
--- router.js
+++ router.js
@@ -1,1 +1,7 @@
 // initialization du routeur et la gestion des routes
+const router = require('express').Router();
+const gameRouter = require('./routers/game.js');
+
+router.use('/games', gameRouter);
+
+module.exports = router;
